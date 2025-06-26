0692a261aabc3f7a61f635dcaa228d3bd31650e4
Caio Madeira <caiomadeira.dev@gmail.com>
2025-06-25 22:27:56 -0300

    fix no compiling with just download pspdev for ubuntu

---
+++ build/CMakeFiles/APPPSP.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "APPPSP"
+  "APPPSP.pdb"
+  "CMakeFiles/APPPSP.dir/include/audio.c.obj"
+  "CMakeFiles/APPPSP.dir/include/audio.c.obj.d"
+  "CMakeFiles/APPPSP.dir/include/player.c.obj"
+  "CMakeFiles/APPPSP.dir/include/player.c.obj.d"
+  "CMakeFiles/APPPSP.dir/main.c.obj"
+  "CMakeFiles/APPPSP.dir/main.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/APPPSP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
